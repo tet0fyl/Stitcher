e035f6ba762d7cdf4fc50178fd8af5bbe8590785
theophile.braye <theophile.braye@gmail.com>
2020-10-31 17:52:43 +0100

    Manage Permission Camera

--- AndroidApp/app/src/main/java/com/example/sticher/controllers/CameraActivity.java
+++ AndroidApp/app/src/main/java/com/example/sticher/controllers/CameraActivity.java
@@ -1,7 +1,12 @@
 package com.example.sticher.controllers;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,26 +16,38 @@ import com.example.sticher.models.CameraPreview;
 
 
 public class CameraActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
         Camera camera = getCameraInstance();
-        CameraPreview cameraPpreview = new CameraPreview(this, camera);
+        CameraPreview cameraPreview = new CameraPreview(this, camera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(cameraPpreview);
+        Button btnCapture = (Button) findViewById(R.id.capture);
+        preview.addView(cameraPreview);
     }
 
-    /** A safe way to get an instance of the Camera object. */
-    public static Camera getCameraInstance(){
+    public Camera getCameraInstance(){
         Camera c = null;
         try {
-            c = Camera.open(); // attempt to get a Camera instance
+            c = Camera.open(0);
         }
         catch (Exception e){
             // Camera is not available (in use or does not exist)
         }
         return c; // returns null if camera is unavailable
     }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
     }
+}
+
--- AndroidApp/app/src/main/java/com/example/sticher/controllers/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/sticher/controllers/MainActivity.java
@@ -1,30 +1,74 @@
 package com.example.sticher.controllers;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.sticher.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean allowSitching = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btnStartCam = (Button) findViewById(R.id.start_cam);
-
+        allowSitching = cameraPermissionRequest();
         btnStartCam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent cameraActivity = new Intent(MainActivity.this,CameraActivity.class);
-                startActivity(cameraActivity);
+                if(allowSitching) {
+                    Intent cameraActivity = new Intent(MainActivity.this,CameraActivity.class);
+                    startActivity(cameraActivity);
+                } else {
+                    Toast.makeText(getApplicationContext(),"Camera Permission required\nCheck App Permission Setting", Toast.LENGTH_LONG).show();
+                    allowSitching = cameraPermissionRequest();
+                }
             }
         });
     }
 
+    public boolean cameraPermissionRequest(){
+
+        int cameraPermission = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA);
+
+        if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.CAMERA,
+                            Manifest.permission.CAMERA},
+                    100
+            );
+            return false;
+        } else {
+            return true;
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 100: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    allowSitching = true;
+                } else {
+                    allowSitching = false;
+                }
+                return;
+            }
+        }
+    }
 
 }
